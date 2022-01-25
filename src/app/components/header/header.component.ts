386b50effd52e9e31d88b72869231e9e88678fc0
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-01-24 22:11:56 -0300

    Header changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,27 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Game } from 'src/app/models/game';
+import { GameListService } from 'src/app/services/game-list.service';
+import classification from 'src/app/shared/classification-items';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
+
 export class HeaderComponent implements OnInit {
+  games: Observable<Game[]>;
+  categories: any;
+  platforms: any;
 
-  categoryIsActive: boolean = false;
-  platformIsActive: boolean = false;
-
-  constructor() { }
+  constructor(private gameListServ: GameListService) {
+    //this.games = this.gameListServ.getAll();
+    // this.platforms = classification.platform;
+  }
 
   ngOnInit(): void {
+    this.platforms = classification.platform;
   }
 
-  categoryClick(){
-    this.categoryIsActive = !this.categoryIsActive
+  dropCategoryIsActive: boolean = false;
+  dropPlatformIsActive: boolean = false;
+
+  public get() {
+    this.gameListServ.get();
   }
 
-  platformClick(){
-    this.platformIsActive = !this.platformIsActive
-
+  categoryClick() {
+    this.dropCategoryIsActive = !this.dropCategoryIsActive;
+    this.dropPlatformIsActive = false;
   }
 
+  platformClick() {
+    this.dropPlatformIsActive = !this.dropPlatformIsActive;
+    this.dropCategoryIsActive = false;
+  }
 }
