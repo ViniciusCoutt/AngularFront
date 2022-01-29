029cfb1723af1f5d6f24e6388b4cbcb078d61612
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-01-28 23:39:16 -0300

    Refactoring: game list component

--- src/app/components/pages/games/game-list/game-list.component.ts
+++ src/app/components/pages/games/game-list/game-list.component.ts
@@ -1,28 +1,22 @@
 import { Component } from '@angular/core';
+import { Game } from '@app/models/game';
 import { GameListService } from '@shared/services/game-list.service';
-import classification from 'src/app/shared/classification-items';
+import { EMPTY, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.scss'],
+
 })
 export class GameListComponent {
+
   games$ = this.gameListServ.games$;
-  platforms: any;
-  release_date: string;
-  categories: any;
-  platSelected: string;
 
 
   constructor(
     private gameListServ: GameListService
-  ) {
-    this.platforms = classification.platforms;
-    this.categories = classification.genres;
-  }
+  ) {}
 
-  // onScrollDown():void {
-  //   console.log("Down");
-  // }
+
 }
--- src/app/components/pages/games/game-list/game-list.module.ts
+++ src/app/components/pages/games/game-list/game-list.module.ts
@@ -1,18 +1,29 @@
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { GameCardModule } from './../game-card/game-card.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SortingItemsModule } from '@games/sorting-items/sorting-items.module';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
+import { LoadingSpinnerModule } from './../../../../shared/components/loading-spinner/loading-spinner.module';
 import { GameListRoutingModule } from './game-list-routing.module';
 import { GameListComponent } from './game-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   imports: [
     CommonModule,
     GameListRoutingModule,
     InfiniteScrollModule,
-    BsDropdownModule
+    SortingItemsModule,
+    LoadingSpinnerModule,
+    GameCardModule,
+    // BrowserModule,
+    // BrowserAnimationsModule
+    // BsDropdownModule,
+
   ],
   declarations: [GameListComponent]
 })
