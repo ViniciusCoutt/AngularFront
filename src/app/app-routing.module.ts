b02e31310d372e51e345eee8ba5778c0b6137907
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-01-28 23:46:32 -0300

    small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
   { path: 'games', loadChildren: () => import('./components/pages/games/game-list/game-list.module').then(m => m.GameListModule) },
-  { path: 'game-details/:id', loadChildren: () => import('./components/pages/games/game-list/game-details/game-details.module').then(m => m.GameDetailsModule) },
+  { path: 'game-details/:id', loadChildren: () => import('./components/pages/games/game-details/game-details.module').then(m => m.GameDetailsModule) },
   { path: '**', loadChildren: () => import('./components/pages/not-found/not-found.module').then(m => m.NotFoundModule) },
 
 ];
