e4c54808bfb03968080c0f38a5beed713bc9ebab
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-01-29 23:47:05 -0300

    Add: up button

--- src/app/components/pages/games/game-list/game-list.component.ts
+++ src/app/components/pages/games/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
 import { Game } from '@app/models/game';
 import { GameListService } from '@shared/services/game-list.service';
 import { EMPTY, Observable } from 'rxjs';
@@ -12,11 +13,27 @@ import { EMPTY, Observable } from 'rxjs';
 export class GameListComponent {
 
   games$ = this.gameListServ.games$;
+  showBtn: boolean = false;
+  private scrollHeight = 500;
 
 
-  constructor(
+  constructor(@Inject(DOCUMENT) private document: Document,
     private gameListServ: GameListService
   ) {}
 
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
+    this.showBtn = (yOffSet || scrollTop) > this.scrollHeight;
+  }
 
+  scrollUp(): void {
+    this.document.documentElement.scrollTop = 0;
+  }
+
+  onScrollDown(): void {
+    this.gameListServ.getGamesByPage(2)
+    console.log("down")
+  }
 }
